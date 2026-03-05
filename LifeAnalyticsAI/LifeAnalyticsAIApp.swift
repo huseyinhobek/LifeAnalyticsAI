c3eea2a3b882dc4dad3c82c17627fbb843ef72c2
Hüseyin Höbek <hsynhbk@icloud.com>
2026-03-06 02:21:43 +0300

    Initial Commit

---
+++ LifeAnalyticsAI/LifeAnalyticsAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeAnalyticsAIApp.swift
+//  LifeAnalyticsAI
+//
+//  Created by Hüseyin Höbek on 6.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct LifeAnalyticsAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
